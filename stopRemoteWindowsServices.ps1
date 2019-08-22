8c513929958de4ac1182f01ed2cf3e46c55b9fed
Sandeep Sadanandan <sandeepmzc@gmail.com>
2019-08-22 16:18:05 +0530

    Create stopRemoteWindowsServices.ps1
    Script to connect to a remote windows machine and stop a windows Server

---
+++ stopRemoteWindowsServices.ps1
@@ -0,0 +1,12 @@
+﻿cls
+$cred = Get-Credential
+$server = 'ip/host'
+$service = 'ServiceName' 
+
+invoke-command -Credential $cred -ComputerName $server -ScriptBlock {
+    param(
+       [Parameter(Mandatory=$True,Position=0)]
+       [string]$service
+    )
+    stop-service $service 
+} -ArgumentList $service
